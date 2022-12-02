a70366ae90e61c6c8d3f388bbd0061c431801ae6
Asplap3277 <mmohammedhamza333@gmail.com>
2022-12-02 11:27:46 +0530

    Implementation on different types of examples on events

--- Events.js
+++ Events.js
@@ -1,10 +1,10 @@
 
-//On Click Event Arrow Function
+/* On Click Event Arrow Function
 document.querySelector("html").addEventListener("click",  ()=> {
     alert("Ouch! Stop poking me!");
   });
 
-  
+
 const myImage = document.querySelector("img")
  myImage.onclick = () => {
     const mysrc = myImage.getAttribute("src")
@@ -16,4 +16,17 @@ const myImage = document.querySelector("img")
     
     myImage.setAttribute("src","Images/pexels-aleksandar-pasaric-2115367.jpg")
   }   
+ }
+ */
+
+ const button = document.querySelector("button")
+ const heading = document.querySelector("h2")
+
+ function User() {
+    const name = prompt("Enter user name")
+    heading.textContent = "This is User name handling event " + name
+ }
+
+ button.onclick = () => {
+    User()
  }
