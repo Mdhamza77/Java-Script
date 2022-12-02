954251c9fbe48b50e7d1ea8ea52c11be42c2bcaa
Asplap3277 <mmohammedhamza333@gmail.com>
2022-12-02 09:50:55 +0530

    Implementation on Variable

---
+++ Variables.js
@@ -0,0 +1,28 @@
+
+const x = {xx:"Christian bale"}
+let  y = 180
+function name() {
+    if(x != null){
+        x.xx = "Christopher"
+    }
+    else if(y == 180){
+       y = 360
+    }
+    console.log(x)
+    console.log(y)
+}
+
+name()
+
+var key = "123"
+if(key == "123"){
+    alert("The Key is matched")
+}
+else {
+    alert("Mismatch")
+}
+
+
+
+
+
