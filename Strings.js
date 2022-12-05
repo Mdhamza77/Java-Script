440420c5b90ad8676f8db65e224a69612ebb095b
Asplap3277 <mmohammedhamza333@gmail.com>
2022-12-05 14:44:28 +0530

    Implementation on different use cases of String

--- Strings.js
+++ Strings.js
@@ -83,3 +83,114 @@ console.log(typeof strprem1)
 console.log(typeof strprem2)
 console.log(typeof strprem3)
 console.log(typeof strobj)
+
+// String MEthods 
+
+// @@Iterators  
+
+const q = "Welcome to Programming languages"
+
+const iterator = q[Symbol.iterator]();
+let Char = iterator.next();
+// value  The next value in the iteration sequence.
+// Done    If value is present alongside done, it is the iterator's return value.
+
+while (!Char.done && Char.value !== ' ') {
+  console.log(Char.value);
+  Char = iterator.next();
+} 
+
+const str = "ABC";
+
+const strIter = str[Symbol.iterator]();
+
+console.log(strIter.next().value); 
+console.log(strIter.next().value); 
+
+// at(): 
+
+let wrd = '12233333 Hamza'
+let cnt  = 4
+console.log(`The index count of ${wrd} at index of ${cnt} is ${wrd.at(cnt)} `)
+
+//charAt() 
+ 
+let nw = "This is to define String values using CharAt()" 
+let cn = 16
+console.log(`The index values of String ${nw} is ${nw.charAt(cn)} `)
+
+//concat() :  
+
+var str1 = "Muhammed"
+var str2 = "Hamza"
+console.log(str1.concat(' ', str2))
+console.log("".concat(...str1))
+console.log("".concat([]))
+
+// Endswith()
+
+var name = "Muhaimin jamal"
+console.log(name.endsWith("jamal"))
+console.log(name.endsWith("dl"))
+
+// includes()
+
+var quote = "In the Bleek Mid Winter there is heavy snow fall"
+console.log(quote.includes('Mid'))
+
+//indexof() && LastofIndex() : 
+
+var quot = "Take a Break and have health time for ur mental health"
+console.log(`The index of ${quot} is at  ${quot.indexOf('health')}`)
+console.log(`The last of index of ${quot} is ${quot.lastIndexOf('health')} `)
+
+// Localecompare
+var pc1 = "wel"
+var pc = "COME"
+console.log(pc.localeCompare(pc1)) 
+
+// match() : 
+
+var pass = "Hellooooo"
+var regex = /[A-Za-z]/g
+console.log(pass.match(regex))
+
+// matchAll() :
+
+var reg = /H(a)(mza(\d{1,9}))/g ;
+var name = "Hamza786Hamza876"
+var arr = [...name.matchAll(reg)]
+console.log(arr[0])
+
+//PadEnd() :
+var ex = "500"
+console.log(ex.padEnd(4,'$'))
+
+// Pad Start() :
+ var cno = '4444455555666'
+ var lno = cno.slice(-3);
+ var pdno = lno.padStart(cno.length,'*')
+ console.log(pdno)
+
+//slice() :
+
+var poem = "Spitting the facts instead"
+var slice = poem.slice(13)
+console.log(slice)
+console.log(poem)
+
+// split() :
+
+var poem = "Spitting the facts instead"
+var wrp = poem.split(' ')
+console.log(wrp[0])
+var cpy = poem.split()
+console.log(cpy)
+var chr = poem.split('')
+console.log(chr[10])
+
+// to String() :  
+
+var strobj1 = new String(1234)
+console.log(strobj1)
+console.log(strobj1.toString())
